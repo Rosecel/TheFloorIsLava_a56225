0f8a253e518273df5b2cb6c3586aefab4b4f4f58
Rosecel <161320383+Rosecel@users.noreply.github.com>
2024-03-09 10:58:52 +0000

    Código no script padrão

--- Assets/HelloWord.cs
+++ Assets/HelloWord.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class HelloWord : MonoBehaviour
 {
+    public string myMessage;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class HelloWord : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(myMessage);
     }
 }

b1c6ba93376f4cf5cd69c145863f336aae528094
Rosecel <161320383+Rosecel@users.noreply.github.com>
2024-03-09 13:43:36 +0000

    Alterar um GameObject com script

--- Assets/PlatformRotate.cs
+++ Assets/PlatformRotate.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWord : MonoBehaviour
+public class PlatformRotate : MonoBehaviour
 {
+    public Vector3 rotateChange;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(rotateChange);
     }
 }
--- Assets/TrackPosition.cs
+++ Assets/TrackPosition.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWord : MonoBehaviour
+public class TrackPosition : MonoBehaviour
 {
+    public Vector3 positionChange;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += positionChange;
     }
 }
